63f023e580c2c578722dd07f2bc2aa0ca80a39ce
Yogesh Giri <yogi.giri263@gmail.com>
2021-05-25 17:02:37 +0530

    Sample.java file modified

--- src/test/java/TestPack/Sample.java
+++ src/test/java/TestPack/Sample.java
@@ -1,12 +1,19 @@
 package TestPack;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class Sample
 {
     @Test
-    public void show()
-    {
-        System.out.println("Test completed");
+    public void show() throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://www.google.com");
+        driver.manage().window().maximize();
+        Thread.sleep(3000);
+        driver.close();
     }
 }
