b93b332b4cd4d707ff08369487faf3fd746fdadb
Yogesh Giri <yogi.giri263@gmail.com>
2021-05-25 14:10:25 +0530

    initial commit

---
+++ src/test/java/TestPack/Sample.java
@@ -0,0 +1,12 @@
+package TestPack;
+
+import org.testng.annotations.Test;
+
+public class Sample
+{
+    @Test
+    public void show()
+    {
+        System.out.println("Test completed");
+    }
+}
